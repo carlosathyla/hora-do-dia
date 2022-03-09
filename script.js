ff0026cee11606f40dd94462310bf3b431a63e6d
Carlos Áthyla Fidelis de Lima <82127079+carlosathyla@users.noreply.github.com>
2022-03-09 01:48:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,28 +4,27 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     var minutos = data.getMinutes()
-    //var hora = 10
-    msg.innerHTML = `Agora sao ${hora} horas e ${minutos} minutos`
+    
+    
     if (hora >= 0 && hora < 6) {
         //Boa madrugada
-        img.src = 'https://carlosathyla.github.io/hora-do-dia/images/madrugada1.png'
+        img.src = 'images/madrugada1.png'
         document.body.style.background = "#e0e0e0"
-        alert('Boa madrugada')
+        msg.innerHTML = `Bom Madrugada! Agora sao ${hora} h e ${minutos} min`
     } else if (hora >= 6 && hora < 12) {
-        //Bom dia
-        img.src = 'https://carlosathyla.github.io/hora-do-dia/images/manha1.png'
+        img.src = 'images/manha1.png'
         document.body.style.background = "#ead56c"
-        alert('Bom dia')
+        msg.innerHTML = `Bom dia! Agora sao ${hora} h e ${minutos} min`
     } else if (hora >= 12 && hora < 18) {
         //Boa tarde
-        img.src = 'https://carlosathyla.github.io/hora-do-dia/images/tarde1.png'
+        img.src = 'images/tarde1.png'
         document.body.style.background = "#fc7f03"
-        alert('Boa tarde')
+        msg.innerHTML = `Bom tarde! Agora sao ${hora} h e ${minutos} min`
     } else {
         //Boa noite
-        img.src = 'https://carlosathyla.github.io/hora-do-dia/images/noite1.png'
+        img.src = 'images/noite1.png'
         document.body.style.background = "#3d1c33"
-        alert('Boa noite')
+        msg.innerHTML = `Bom noite! Agora sao ${hora} h e ${minutos} min`
     }
 }
 
